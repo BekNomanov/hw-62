d41bdd4b33aa3d43fbe8bf2d5b7134d95d419cff
Bekbolot Nomanov <nomanovbekbolot7@gmail.com>
2024-02-06 16:41:04 +0600

    Добавил  статью

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {Routes, Route, NavLink} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const App : React.FC = () => {
+const App: React.FC = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <header className='container'>
+        <nav className="navbar bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <NavLink to="/">Домашняя</NavLink>
+            <NavLink to="/about">Пилоты</NavLink>
+            <NavLink to="/contacts">Контакты</NavLink>
+          </div>
+        </nav>
+      </header>
+      <main className='container'>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contacts" element={<Contacts/>}/>
+        </Routes>
+      </main>
+    </>
   );
 };
 
